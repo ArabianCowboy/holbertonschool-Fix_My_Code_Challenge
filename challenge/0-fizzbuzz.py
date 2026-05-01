41e221ea3cc1f4ff4234a0a6d41f5ea1f5c4016f
Mohammed <midoxp@gmail.com>
2026-05-01 19:16:12 +0300

    Refactor fizzbuzz function to use a consistent end parameter for print statements

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -3,14 +3,15 @@ import sys
 
 def fizzbuzz(n):
     for i in range(1, n + 1):
+        end = "\n" if i == n else " "
         if i % 15 == 0:
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz", end=end)
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz", end=end)
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            print("Buzz", end=end)
         else:
-            print(i, end=" ")
+            print(i, end=end)
 
 if __name__ == "__main__":
     fizzbuzz(int(sys.argv[1]))
